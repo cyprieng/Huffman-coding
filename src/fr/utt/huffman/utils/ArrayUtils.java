1961b3f2b2c6f0ffc5b1fb2b5c0524166d1de60e
cyprieng <cyprien.guillemot@gmail.com>
2013-11-22 12:51:31 +0100

    Add ArrayUtils class

---
+++ src/fr/utt/huffman/utils/ArrayUtils.java
@@ -0,0 +1,32 @@
+package fr.utt.huffman.utils;
+
+/**
+ * Class managing small handling on arrays
+ * 
+ * @author cyprien
+ * 
+ */
+public class ArrayUtils {
+	/**
+	 * Delete the 0 values of a sorted array (without negative value)
+	 * 
+	 * @param array
+	 *            The sorted array where we want to proceed
+	 * @return An array without 0
+	 */
+	public static int[] deleteZero(int[] array) {
+		// We scan the array
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != 0) {
+				// The value is not 0, and the array is sorted. So we just keep
+				// the end of the array
+				int[] arrayWithoutZero = new int[array.length - i - 1];
+				System.arraycopy(array, i, arrayWithoutZero, 0,
+						arrayWithoutZero.length);
+				return arrayWithoutZero;
+			}
+		}
+
+		return null;
+	}
+}
